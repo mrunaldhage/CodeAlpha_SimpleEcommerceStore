64ce259c0c441a7e065ded4eaa540dc7afb00e6f
mrunaldhage <mrunaldhage2307@gmail.com>
2026-02-09 14:38:03 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 1999,
+    image: "images/headphone.jpg"
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 2999,
+    image: "images/watch.jpg"
+  },
+  {
+    id: 3,
+    name: "Bluetooth Speaker",
+    price: 1499,
+    image: "images/speaker.jpg"
+  }
+];
